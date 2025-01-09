9fabc8a8f56f24bbf942f998394371829e248707
genomicinvader <anikangel664@gmail.com>
2025-01-09 11:49:34 +0600

    Switch statement added with default value.

---
+++ switch_statement.php
@@ -0,0 +1,19 @@
+<?php
+$number = 100;
+switch ($number) {
+  case 34:
+    echo "It is 34";
+    break;
+  case 35:
+    echo "It is 35";
+    break;
+  case 36:
+    echo "It is 36";
+    break;
+  case 100:
+    echo "It is 100";
+    break;
+  default:
+    echo "It is not a 34, 35, 36, or 37";
+}
+?>
