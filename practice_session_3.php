68a2513fbd5d2058b3c540fd22d970216192e50e
genomicinvader <anikangel664@gmail.com>
2025-01-10 19:56:41 +0600

    I love PHP statement is created

--- practice_session_3.php
+++ practice_session_3.php
@@ -8,4 +8,16 @@ challenge 2: Make a for loop that displays 10 numbers
 challenge 3: make a switch statement that tests against 1 conditions with 5 cases
 
 */
+
+
+//Challenge 1
+$love_php = true;
+if ($love_php = true) {
+echo "I love PHP";}
+else if($love_php = false){
+  echo "I don't love PHP";
+}
+else{
+  echo "I hate love PHP";
+}
 ?>
