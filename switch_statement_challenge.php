e25b8d2650b8995a0f3e561abc415c7084d06dc2
genomicinvader <anikangel664@gmail.com>
2025-01-10 22:33:42 +0600

    switch statement challenge completed

---
+++ switch_statement_challenge.php
@@ -0,0 +1,20 @@
+<?php
+$number = 500;
+
+switch ($number) {
+  case 200:
+    echo "It is 200";
+    break;
+  case 300:
+    echo "It is 300";
+    break;
+  case 400:
+    echo "It is 400";
+    break;
+  case 500:
+    echo "It is 500";
+    break;
+  default:
+    echo "It is unknown";
+}
+?>
