26bdc2834545489192e7aba39e2be23d52d953a4
genomicinvader <anikangel664@gmail.com>
2025-01-10 19:43:08 +0600

    practice session 3 challenges updated

---
+++ practice_session_3.php
@@ -0,0 +1,11 @@
+<?php
+/*
+challenge 1: make an if statement with elseif statement and else to finally show / display saying "I love PHP"
+
+challenge 2: Make a for loop that displays 10 numbers
+
+
+challenge 3: make a switch statement that tests against 1 conditions with 5 cases
+
+*/
+?>
